9e8c7b69d693143918e92ea5a2a991f7bb6e64f7
ohnapaula <ohnapaula@gmail.com>
2025-09-13 02:21:12 -0300

    fix: corrige escolha de operação inválida

--- sistema_bancario/main.py
+++ sistema_bancario/main.py
@@ -7,15 +7,19 @@ def main ():
         print('[2] - DEPÓSITO')
         print('[3] - EXTRATO')
         print('[4] - SAIR')
-        opcao = int(input('Digite o número da operação: '))
-        if opcao == 1:
-            pass    # sacar
-        elif opcao == 2:
-            pass    # depositar
-        elif opcao == 3:
-            pass    # visualizar extrato
-        elif opcao == 4:
-            print('Encerrando aplicação...')
-        else:
-            print('Opção inválida!')
+        try:
+            opcao = int(input('Digite o número da operação: '))
+            if opcao == 1:
+                pass    # sacar
+            elif opcao == 2:
+                pass    # depositar
+            elif opcao == 3:
+                pass    # visualizar extrato
+            elif opcao == 4:
+                print('Encerrando aplicação...')
+            else:
+                print('Opção inválida!')
+        except ValueError:
+            print('ERRO: INFORME UM VALOR INTEIRO.')
+
 main()
