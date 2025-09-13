ada353370198f5027787ed3619c2bc6a3030f2a5
ohnapaula <ohnapaula@gmail.com>
2025-09-13 01:06:52 -0300

    feat: adiciona menu de operações

---
+++ sistema_bancario/main.py
@@ -0,0 +1,21 @@
+# Desafio DIO
+def main ():
+    opcao = -1
+    while opcao != 4:
+        print('##### SISTEMA BANCÁRIO DIO #####')
+        print('[1] - SAQUE')
+        print('[2] - DEPÓSITO')
+        print('[3] - EXTRATO')
+        print('[4] - SAIR')
+        opcao = int(input('Digite o número da operação: '))
+        if opcao == 1:
+            pass    # sacar
+        elif opcao == 2:
+            pass    # depositar
+        elif opcao == 3:
+            pass    # visualizar extrato
+        elif opcao == 4:
+            print('Encerrando aplicação...')
+        else:
+            print('Opção inválida!')
+main()
